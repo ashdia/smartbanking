fc20fc554d215383f480dfef2d94677dfbd558ce
ashdia <song_nuaa@yahoo.com>
2016-06-12 23:39:34 -0600

    add dist folder

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ module.exports = {
   app: function () {
     const app = express();
     const indexPath = path.join(__dirname, '/dist/index.html');
-    const publicPath = express.static(path.join(__dirname, '/'));
+    const publicPath = express.static(path.join(__dirname, '/dist'));
 
     app.use('/', publicPath);
     app.use(/.*/, function (_, res) { res.sendFile(indexPath) });
