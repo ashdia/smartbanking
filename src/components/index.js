0855692842b5b253ef720a03bed494333b669e09
ashdia <song_nuaa@yahoo.com>
2016-06-20 22:48:39 -0600

    CRM demo, add build pkg

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ export { default as TextInput } from './common/textInputComponent.js';
 export { default as Button } from './common/buttonComponent.js';
 export { default as CheckBox } from './common/checkBoxComponent.js';
 export { default as Dropdown } from './common/dropdownComponent.js';
-export { default as Icon } from './common/IconComponent.js';
+export { default as Icon } from './common/iconComponent.js';
 export { default as Popup } from './common/popupComponent.js';
 export { default as SearchBox } from './common/searchBoxComponent.js';
 export { default as Table } from './common/tableComponent.js';
